f87d9938a921f9dff362894036993673fcd11c80
Jocelyn Naarden <tzionone@gmail.com>
2022-08-18 01:56:10 -0400

    Works but wont pass tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,11 +2,12 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    students = Student.find_by("lower(first_name) = ? OR lower(last_name) = ? ", params[:name].downcase, params[:name].downcase) if params[:name].present?
     render json: students
   end
 
   def show
-    student = Student.all.find(params[:id])
+    student = Student.find(params[:id])
     render json: student
   end
 
